2364c041a45d2ed8bc124ce2550e002dc78f8a2d
David Schwartz <33384202+DavSchwartz@users.noreply.github.com>
2021-01-04 18:08:40 -0600

    Modify zero-g code

--- Player.gd
+++ Player.gd
@@ -1,37 +1,34 @@
 extends KinematicBody2D
 
-const MOVE_SPEED = 500
-const JUMP_FORCE = 1000
-const GRAVITY = 10
-const MAX_FALL_SPEED = 1000
-const AntiGravityEnabled = false
+const MOVE_SCALAR: int = 10
+const JUMP_SCALAR: int = 10
 
-onready var sprite = $Sprite
+var velocity := Vector2(0, 1)
+var tile := preload("res://TileMap.gd")
+var floorNormal := Vector2(0, 0)
+var grounded := false
 
-var y_velo = 0
-var facing_right = false
-
-func _physics_process(delta):
-	var move_dir = 0
-	if Input.is_action_pressed("move_right"):
-		move_dir += 1
-	if Input.is_action_pressed("move_left"):
-		move_dir -= 1
-	move_and_slide(Vector2(move_dir * MOVE_SPEED, y_velo), Vector2(0, -1))
+func _physics_process(delta: float):
+	if grounded:
+		var moveDir := Vector2(0, 0)
+		if Input.is_action_pressed("move_right"):
+			moveDir.x += 1
+		if Input.is_action_pressed("move_left"):
+			moveDir.x -= 1
+		if Input.is_action_pressed("move_down"):
+			moveDir.y += 1
+		if Input.is_action_pressed("move_up"):
+			moveDir.y -= 1
+			
+		var moveTransform: Vector2 = floorNormal.rotated(PI/2).abs()
+		velocity += moveDir * moveTransform * MOVE_SCALAR * delta
 	
-	var groundeddown = is_on_floor()
-	var groundedup = is_on_ceiling()
-	print(groundeddown)
-	y_velo += GRAVITY
-	if groundeddown and Input.is_action_just_pressed("jump"):
-		y_velo = -JUMP_FORCE
-	if groundedup:
-		y_velo = -500
-		if groundedup and Input.is_action_just_pressed("jump"):
-			y_velo = JUMP_FORCE	
-	if y_velo > MAX_FALL_SPEED:
-		y_velo = MAX_FALL_SPEED
+		if Input.is_action_just_pressed("jump"):
+			velocity += floorNormal * JUMP_SCALAR
+			grounded = false
 	
-
-func _on_AntiGravitychecker_body_entered(body):
-	pass # Replace with function body.
+	var collision: KinematicCollision2D = move_and_collide(velocity)
+	if (collision && (collision.get_collider() is tile)):
+		grounded = true
+		floorNormal = collision.get_normal()
+		velocity -= velocity * floorNormal.abs()
