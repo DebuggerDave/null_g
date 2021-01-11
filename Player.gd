89860c1279dac08407b6ab0b17ebb9a6853bc49d
David Schwartz <dmschwartz01@gmail.com>
2021-01-10 22:13:23 -0600

    working kinematic body prototype, added camera, horrible spaghetti code

--- Player.gd
+++ Player.gd
@@ -8,7 +8,7 @@ const MAX_SNAP_ANGLE: float = PI / 4
 var tileMap := preload("res://TileMap.gd")
 var grounded := false
 var floorNormal := Vector2()
-var velocity := Vector2(0, -100)
+var velocity := Vector2(0, 100)
 var lastCollision := Vector2()
 var rayCast := RayCast2D.new()
 
@@ -32,8 +32,8 @@ func _physics_process(delta: float) -> void:
 			movement = collision.get_remainder()
 			movement -= movement * floorNormal.abs()
 			velocity -= velocity * floorNormal.abs()
-			if (lastCollision != floor_normal_border_point(collision.position)):
-				var lastMovement: Vector2 = floor_normal_border_point(collision.position) - lastCollision
+			if (lastCollision != centered_floor_contact_point(collision.position)):
+				var lastMovement: Vector2 = centered_floor_contact_point(collision.position) - lastCollision
 				grounded = false
 				movement += query_floor(lastMovement)
 		else:
@@ -46,19 +46,25 @@ func _physics_process(delta: float) -> void:
 func query_floor(lastMovement: Vector2) -> Vector2:
 	var movement := Vector2()
 	
+	
+	
 	# Look torwards floor
-	var rayEnd: Vector2 = lastCollision + lastMovement
-	var rayStart: Vector2 = rayEnd + floorNormal
+	var rayEnd: Vector2 = lastCollision + lastMovement - floorNormal
+	var rayStart: Vector2 = lastCollision + lastMovement + floorNormal
 	rayCast.set_cast_to(rayEnd - get_position())
 	rayCast.position = rayStart - get_position()
 	rayCast.force_raycast_update()
-	if (rayCast.is_colliding() &&
-		(rayCast.get_collider() is tileMap) &&
-		(rayCast.get_collision_normal() == floorNormal) &&
+	var ah = rayCast.is_colliding()
+	var ahh = rayCast.get_collider() is tileMap
+	var ahhh = rayCast.get_collision_normal() == floorNormal
+	
+	var space_state = get_world_2d().direct_space_state
+	var result: Dictionary = space_state.intersect_ray(rayStart, rayEnd, [self])
+	if ((not result.empty()) &&
+		(result["collider"] is tileMap) &&
 		(not grounded)
 	):
-		floorNormal = rayCast.get_collision_normal()
-		lastCollision = get_actual_collision_point()
+		lastCollision = line_intersection(floorNormal.rotated(PI/2), lastCollision, floorNormal, result["position"])
 		grounded = true
 	else:
 		pass
@@ -69,18 +75,27 @@ func query_floor(lastMovement: Vector2) -> Vector2:
 	rayCast.set_cast_to(rayEnd - get_position())
 	rayCast.position = rayStart - get_position()
 	rayCast.force_raycast_update()
-	if (rayCast.is_colliding() && (not grounded)):
-		var rotation: float = floorNormal.angle_to(rayCast.get_collision_normal())
-		if ((abs(rotation) <= MAX_SNAP_ANGLE) && (rayCast.get_collider() is tileMap)):
-			set_rotation(get_rotation() + rotation)
-			floorNormal = rayCast.get_collision_normal()
-			lastCollision = get_actual_collision_point()
+	result = space_state.intersect_ray(rayStart - floorNormal, rayEnd - floorNormal, [self])
+	if ((not result.empty()) && (not grounded)):
+		var rotation: float = floorNormal.angle_to(result["normal"])
+		if ((abs(rotation) <= MAX_SNAP_ANGLE) && (result["collider"] is tileMap)):
+			var rotationTransform: Transform2D = Transform2D(rotation, Vector2())
+			var lastFloorNormal: Vector2 = floorNormal
+			floorNormal = result["normal"]
+			lastCollision = line_intersection(floorNormal.rotated(PI/2), result["position"], lastFloorNormal.rotated(PI/2), lastCollision)
+			var adjustForRotation: Vector2 = rayStart - (rotationTransform.basis_xform(rayStart - get_position()) + get_position())
 			var moveBack: Vector2 = lastCollision - rayStart
-			move_and_collide(moveBack)
+			set_rotation(get_rotation() + rotation)
+			set_position(get_position() + adjustForRotation + moveBack)
 			movement -= moveBack
 			movement = movement.rotated(rotation)
 			velocity = velocity.rotated(rotation)
+
 			grounded = true
+			
+			var randomAngle: float = floorNormal.angle_to(Vector2.DOWN)
+			movement = (movement.rotated(randomAngle) - (floorNormal.rotated(randomAngle) * movement.rotated(randomAngle))).rotated(-randomAngle)
+			velocity = (velocity.rotated(randomAngle) - (floorNormal.rotated(randomAngle) * velocity.rotated(randomAngle))).rotated(-randomAngle)
 			
 	return movement
 
@@ -88,26 +103,43 @@ func query_floor(lastMovement: Vector2) -> Vector2:
 func get_actual_collision_point() -> Vector2:
 	var rayEnd: Vector2 = rayCast.get_cast_to() + get_position()
 	var rayStart: Vector2 = rayCast.position + get_position()
-	return line_intersection(rayCast.get_collision_normal().rotated(PI/2), rayCast.get_collision_point(), rayStart, rayEnd - rayStart)
+	return line_intersection(
+		rayCast.get_collision_normal().rotated(PI/2),
+		rayCast.get_collision_point(),
+		rayEnd - rayStart,
+		rayStart
+	)
 
 func line_intersection(direction1: Vector2, point1: Vector2, direction2: Vector2, point2: Vector2) -> Vector2:
 	var intersection := Vector2()
 	
-	var slope1: float = direction1.y / direction1.x
-	var slope2: float = direction2.y / direction2.x
-	var intercept1: float = point1.y - (slope1 * point1.x)
-	var intercept2: float = point2.y - (slope2 * point2.x)
-	intersection.x = (intercept2 - intercept1) / (slope1 - slope2)
-	intersection.y = (intersection.x * slope1) + intercept1
+	if (direction1.is_equal_approx(Vector2(0, direction1.y)) && direction2.is_equal_approx(Vector2(0, direction2.y))):
+		intersection = Vector2.INF
+	elif (direction1.is_equal_approx(Vector2(0, direction1.y)) && (not direction2.is_equal_approx(Vector2(0, direction2.y)))):
+		var slope2: float = direction2.y / direction2.x
+		var intercept2: float = point2.y - (slope2 * point2.x)
+		intersection.x = point1.x
+		intersection.y = (intersection.x * slope2) + intercept2
+	elif ((not direction1.is_equal_approx(Vector2(0, direction1.y))) && direction2.is_equal_approx(Vector2(0, direction2.y))):
+		var slope1: float = direction1.y / direction1.x
+		var intercept1: float = point1.y - (slope1 * point1.x)
+		intersection.x = point2.x
+		intersection.y = (intersection.x * slope1) + intercept1
+	else:
+		var slope1: float = direction1.y / direction1.x
+		var slope2: float = direction2.y / direction2.x
+		var intercept1: float = point1.y - (slope1 * point1.x)
+		var intercept2: float = point2.y - (slope2 * point2.x)
+		if (slope1 == slope2):
+			intersection = Vector2.INF
+		else:
+			intersection.x = (intercept2 - intercept1) / (slope1 - slope2)
+			intersection.y = (intersection.x * slope1) + intercept1
 	
 	return intersection
 
-func floor_normal_border_point(borderPoint: Vector2) -> Vector2:
-	var transformed_border_point: Vector2 = borderPoint * floorNormal.abs()
-	var transformed_position: Vector2 = get_position() * floorNormal.rotated(PI/2).abs()
-	var x: float = sqrt(pow(transformed_border_point.x, 2) + pow(transformed_position.x, 2))
-	var y: float = sqrt(pow(transformed_border_point.y, 2) + pow(transformed_position.y, 2))
-	return Vector2(x, y)
+func centered_floor_contact_point(borderPoint: Vector2) -> Vector2:
+	return line_intersection(floorNormal.rotated(PI/2).abs(), borderPoint, floorNormal.abs(), get_position())
 
 func aggregate_floor_normals(floorNormal: Vector2) -> Vector2:
 	var aggregate := Vector2(floorNormal.x, floorNormal.y)
@@ -135,10 +167,12 @@ func get_new_velocity(delta: float) -> void:
 			moveDir.y -= 1
 		
 		var rotateX: float = Vector2.DOWN.angle_to(floorNormal)
+		rotateX = wrapf(rotateX, -PI/2, PI/2)
 		if (rotateX > PI/2):
 				rotateX = (PI/2) - rotateX
 				
 		var rotateY: float = Vector2.RIGHT.angle_to(floorNormal)
+		rotateY = wrapf(rotateY, -PI/2, PI/2)
 		if (rotateY > PI/2):
 				rotateY = (PI/2) - rotateX
 		
