f82e350dbe4230cd6b3a6b6cd000e99c15b6f2ed
David Schwartz <dmschwartz01@gmail.com>
2021-01-14 10:21:56 -0600

    finish refactor, make art 10x resoltion, add player debug line

--- Player.gd
+++ Player.gd
@@ -1,16 +1,19 @@
 extends KinematicBody2D
 
-const ACCELERATION: int = 100
-const MAX_SPEED: int = 1000
+const ACCELERATION: int = 1000
+const MAX_SPEED: int = 10000
 const JUMP_DISTANCE: int = 10
-const MAX_SNAP_ANGLE: float = PI / 4
+const MAX_SNAP_ANGLE: float = TAU / 8 
 
 var tileMap := preload("res://TileMap.gd")
 var grounded := false
 var floorNormal := Vector2()
 var velocity := Vector2(0, 100)
 var lastCollision := Vector2()
-	
+
+# TODO debug line, remove later
+onready var line = get_node("DEBUG_LINE")
+
 func _physics_process(delta: float) -> void:
 	if grounded:
 		get_new_velocity(delta)
@@ -18,13 +21,21 @@ func _physics_process(delta: float) -> void:
 	
 	while (not movement.is_equal_approx(Vector2())):
 		var collision: KinematicCollision2D = move_and_collide(movement)
+		
 		if (collision && (collision.get_collider() is tileMap)):
 			grounded = true
-			floorNormal = collision.get_normal()
 			lastCollision = collision.position
+			floorNormal = collision.get_normal()
 			movement = collision.get_remainder()
-			movement -= movement * floorNormal.abs()
-			velocity -= velocity * floorNormal.abs()
+			
+			# Cancel out movement into floor
+			if (move_dir().is_equal_approx(Vector2())):
+				movement = Vector2()
+				velocity = Vector2()
+			else:
+				movement = movement.project(move_dir())
+				velocity = velocity.project(move_dir())
+				
 			if (lastCollision != centered_floor_contact_point(collision.position)):
 				var lastMovement: Vector2 = centered_floor_contact_point(collision.position) - lastCollision
 				grounded = false
@@ -37,48 +48,60 @@ func _physics_process(delta: float) -> void:
 				movement += query_floor(lastMovement)
 
 func query_floor(lastMovement: Vector2) -> Vector2:
-	var movement := Vector2()
 	var space_state = get_world_2d().direct_space_state
+	var errorMargin: Vector2 = -floorNormal
 	
-	# Look torwards floor
-	var rayEnd: Vector2 = lastCollision + lastMovement - floorNormal
+	# Look below
+	var rayEnd: Vector2 = lastCollision + lastMovement + errorMargin
 	var rayStart: Vector2 = lastCollision + lastMovement + floorNormal
 	var result: Dictionary = space_state.intersect_ray(rayStart, rayEnd, [self])
-	if ((not result.empty()) &&
+	if (
+		(not result.empty()) &&
 		(result["collider"] is tileMap) &&
+		floorNormal.is_equal_approx(result["normal"]) &&
 		(not grounded)
 	):
-		lastCollision = line_intersection(floorNormal.rotated(PI/2), lastCollision, floorNormal, result["position"])
+		lastCollision = result["position"]
+		floorNormal = result["normal"]
 		grounded = true
-	else:
-		pass
+		return Vector2()
 	
-	# Look back
-	rayEnd = lastCollision
-	rayStart = rayEnd + lastMovement
-	result = space_state.intersect_ray(rayStart - floorNormal, rayEnd - floorNormal, [self])
+	# Look back towards last collision
+	rayEnd = lastCollision + errorMargin
+	rayStart = lastCollision + lastMovement
+	result = space_state.intersect_ray(rayStart, rayEnd, [self])
 	if ((not result.empty()) && (not grounded)):
-		var rotation: float = floorNormal.angle_to(result["normal"])
-		if ((abs(rotation) <= MAX_SNAP_ANGLE) && (result["collider"] is tileMap)):
-			var rotationTransform: Transform2D = Transform2D(rotation, Vector2())
-			var lastFloorNormal: Vector2 = floorNormal
+		var rads: float = floorNormal.angle_to(result["normal"])
+		if ((abs(rads) <= MAX_SNAP_ANGLE) && (result["collider"] is tileMap)):
+			# Correct collision position for added errorMargin
+			# Correct position is the intersection of our current plane and the next one
+			lastCollision = line_intersection(move_dir().rotated(rads), result["position"], move_dir(), lastCollision)
 			floorNormal = result["normal"]
-			lastCollision = line_intersection(floorNormal.rotated(TAU/4), result["position"], lastFloorNormal.rotated(TAU/4), lastCollision)
-			var adjustForRotation: Vector2 = rayStart - (rotationTransform.basis_xform(rayStart - get_position()) + get_position())
+			
+			var rotation: Transform2D = Transform2D(rads, Vector2())
+			var rotationAdjust: Vector2 = rayStart - (get_transform() * rotation * get_transform().inverse() * rayStart)
 			var moveBack: Vector2 = lastCollision - rayStart
-			set_rotation(get_rotation() + rotation)
-			set_position(get_position() + adjustForRotation + moveBack)
-			movement -= moveBack
-			movement = movement.rotated(rotation)
-			velocity = velocity.rotated(rotation)
-
+			set_rotation(get_rotation() + rads)
+			set_position(get_position() + rotationAdjust + moveBack)
+			
+			var movement: Vector2 = -moveBack
+			movement = movement.rotated(rads)
+			velocity = velocity.rotated(rads)
+			movement = movement.project(move_dir())
+			velocity = velocity.project(move_dir())
+			
 			grounded = true
+			return movement
 			
-			var randomAngle: float = floorNormal.angle_to(Vector2.DOWN)
-			movement = (movement.rotated(randomAngle) - (floorNormal.rotated(randomAngle) * movement.rotated(randomAngle))).rotated(-randomAngle)
-			velocity = (velocity.rotated(randomAngle) - (floorNormal.rotated(randomAngle) * velocity.rotated(randomAngle))).rotated(-randomAngle)
-			
-	return movement
+	return Vector2()
+
+func move_dir(turnTowards: Vector2 = velocity) -> Vector2:
+	if (floorNormal.angle_to(turnTowards) > 0):
+		return floorNormal.rotated(TAU/4)
+	if (floorNormal.angle_to(turnTowards) < 0):
+		return floorNormal.rotated(-TAU/4)
+	else:
+		return Vector2()
 
 func line_intersection(direction1: Vector2, point1: Vector2, direction2: Vector2, point2: Vector2) -> Vector2:
 	var intersection := Vector2()
@@ -112,9 +135,9 @@ func infinite_slope(slope: Vector2) -> bool:
 	return slope.is_equal_approx(Vector2(0, slope.y))
 
 func centered_floor_contact_point(borderPoint: Vector2) -> Vector2:
-	return line_intersection(floorNormal.rotated(PI/2).abs(), borderPoint, floorNormal.abs(), get_position())
+	return line_intersection(floorNormal.rotated(TAU/4), borderPoint, floorNormal, get_position())
 
-func aggregate_floor_normals(floorNormal: Vector2) -> Vector2:
+func aggregate_floor_normals() -> Vector2:
 	var aggregate := Vector2(floorNormal.x, floorNormal.y)
 	var original_direction: Vector2 = floorNormal
 	
@@ -137,17 +160,12 @@ func get_new_velocity(delta: float) -> void:
 			moveDir.y += 1
 		if Input.is_action_pressed("move_up"):
 			moveDir.y -= 1
-		
-		var movementDir: Vector2 = floorNormal.rotated(TAU/4).abs()
-		
-		var alignmentAngleX: float = wrapf(Vector2.RIGHT.angle_to(movementDir), -PI/2, PI/2)
-		var alignmentAngleY: float = wrapf(Vector2.DOWN.angle_to(movementDir), -PI/2, PI/2)
 
-		var delta_velocity: float = ACCELERATION * delta
-		var scale := Transform2D(Vector2((movementDir.x * delta_velocity), 0), Vector2(0, (movementDir.y * delta_velocity)), Vector2())
-		var rotate := Transform2D(Vector2(cos(alignmentAngleX), -sin(alignmentAngleX)), Vector2(sin(alignmentAngleY), cos(alignmentAngleY)), Vector2())
-		velocity += rotate * scale * moveDir
+		if ((not moveDir.is_equal_approx(Vector2(0, 0))) && (floorNormal.angle_to(moveDir) != 0)):
+			var delta_velocity: float = ACCELERATION * delta
+			var scale := Transform2D(Vector2((move_dir(moveDir).abs().x * delta_velocity), 0), Vector2(0, (move_dir(moveDir).abs().y * delta_velocity)), Vector2())
+			velocity += (scale * moveDir).project(move_dir(moveDir))
 			
 		if Input.is_action_just_pressed("jump"):
 			grounded = false
-			velocity += aggregate_floor_normals(floorNormal) * JUMP_DISTANCE / delta
+			velocity += aggregate_floor_normals() * JUMP_DISTANCE / delta
